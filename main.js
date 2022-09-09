cc77532e5ded8e1b1952a25ec53da099fd3d6fe8
Mitchellakr <103033728+Mitchellakr@users.noreply.github.com>
2022-09-09 20:08:40 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let countDownDate = new Date("December 31, 2022 15:37:25").getTime();
+
+let x = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (days < 10) {
+        days = "0" + days;
+    }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    document.querySelector("#days").innerHTML = days;
+    document.querySelector("#hours").innerHTML = hours;
+    document.querySelector("#minutes").innerHTML = minutes;
+    document.querySelector("#seconds").innerHTML = seconds;
+}, 1000);
